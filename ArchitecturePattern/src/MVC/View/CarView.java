e120746bcaf77a44c264e21887e27bbdafa93ea5
CarlosHeroku <stelliox.s.l@gmail.com>
2016-05-12 22:56:56 -0500

    View Example

---
+++ ArchitecturePattern/src/MVC/View/CarView.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC.View;
+
+/**
+ *
+ * @author Stelliox.com
+ */
+public class CarView 
+{
+    public void printCarDetails(String brand, String model, String plate)
+    {
+        System.out.println("CAR");
+            System.out.println("Brand: "+brand);
+            System.out.println("Model: "+model);
+            System.out.println("Plate: "+plate);
+    }
+}
